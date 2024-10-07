b2ab74f29f9a6c517c90789b2f5b4c0bd2d7b264
whosubrajit <subraajit@gmail.com>
2024-10-07 21:54:32 +0600

    Update and rename test22.java to test2b.java

--- Assignment 2/test2b.java
+++ Assignment 2/test2b.java
@@ -1,6 +1,6 @@
 //TASK 2 (2.2)
 
-public class test22 {
+public class test2b {
     public static void main(String[] args) {
                 int x = 50; // Declaring the integer value
                 int y = 60; // Declaring the second integer
