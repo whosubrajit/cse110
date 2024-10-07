e3359c20ae6ded80b27578e0eff457599eaeeea5
whosubrajit <subraajit@gmail.com>
2024-10-07 21:55:34 +0600

    Update and rename test26.java to test2f.java

--- Assignment 2/test2f.java
+++ Assignment 2/test2f.java
@@ -1,9 +1,9 @@
 //TASK 2 (2.6) 
 
-public class test26 {
+public class test2f {
     public static void main(String[] args) {
         int x= 110;
- String str1="This is ";    
+        String str1="This is ";    
         String str2="CSE";
         System.out.println(str1+str2);
         System.out.println(str1+str2+x);
